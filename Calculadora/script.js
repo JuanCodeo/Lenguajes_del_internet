e6218d12106889dc83b9f5314c8ed76567d2a645
Juan Jose Guerrero Acosta <jjoseguerrero@ucompensar.edu.co>
2025-09-17 19:46:00 -0500

    Agregamos area de productos

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -22,8 +22,35 @@ function calculadora()
     }
     else if(operacion == "/")
     {
+        if(numero_2 == 0)
+        {
+            alert("Error no se puede dividir por 0")
+            return;
+        }
         resultado = numero_1/numero_2;
         console.log("El resultado de la división es: "+resultado)
     }
     //console.log(numero_1, numero_2);
 }
+
+const productos =
+[
+    {id:1, name:"Producto_1", price:50},
+    {id:2, name:"Producto_2", price:150},
+    {id:3, name:"Producto_3", price:250},
+    {id:4, name:"Producto_4", price:350}
+];
+
+function Mostrar_Productos()
+{
+    const productos_div = document.getElementById("Productos");
+    productos.forEach(producto => 
+    {
+        const producto_div = document.createElement('div');
+        producto_div.innerHTML = `<span>${producto.name} - ${producto.price}</span>
+        <button>Agregar al carrito</button>`;
+        productos_div.appendChild(producto_div);
+    });
+}
+
+Mostrar_Productos();
