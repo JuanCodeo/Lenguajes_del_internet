ab7d8470926e612b24705b0fb5f7f7aac0c75e2f
Juan Jo <130870818+JuanCodeo@users.noreply.github.com>
2025-09-18 17:47:33 -0500

    Taller de Desplegable

---
+++ Taller/script.js
@@ -0,0 +1,26 @@
+// Estos son los elementos del html
+const Boton_Abrir = document.getElementById("Abrir_Modal");
+const Boton_Cerrar = document.getElementById("Cerrar_Modal");
+const Desplegable = document.getElementById("Modal_Overlay");
+
+// aqui se hace la función para que al dar click en el Boton_Abrir lo haga 
+function Abrir()
+{
+    Boton_Abrir.addEventListener("click", () => 
+    {
+        Desplegable.classList.add("visible");
+    });
+}
+Abrir() //Aqui la activamos 
+
+// Estos es lo opuesto al dar click que esto lo cierre 
+function Cerrar()
+{
+    Boton_Cerrar.addEventListener("click", () => 
+    {
+        Desplegable.classList.remove("visible");
+    });
+}
+Cerrar()
+
+
