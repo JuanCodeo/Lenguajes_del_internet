398df45fbd4adc818806519aea66e1a9befe790e
Juan Jo <130870818+JuanCodeo@users.noreply.github.com>
2025-09-25 19:46:43 -0500

    Comentario

--- Silider_Fotos/script.js
+++ Silider_Fotos/script.js
@@ -10,6 +10,7 @@ function Siguiente_Img()
 function Actualizar_Posicion()
 {
     Slider.style.transform = `translateX(-${index * 100}%)`;
+    //Esto es para que se mueva en el eje X
 }
 
 setInterval(Siguiente_Img, 3000); //Aqui se le asigna el tiempo de cambio 
